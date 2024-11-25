86899907fdec93ad7e239522d927c631eb6403c5
SamueleTerraroli <terraroli99@gmail.com>
2024-11-25 21:48:37 +0100

    feat: add riepilogo

--- assets/script/script.js
+++ assets/script/script.js
@@ -5,6 +5,31 @@ form.addEventListener('submit', (event) => { //prevengo l'invio del form prima c
      const fullName = document.getElementById('fullName').value;
      const distance = Number(document.getElementById('distance').value);
      const age = document.getElementById('age').value;
+     const ticketName = document.querySelector('.name-ticket');
+     const ticketType = document.querySelector('.ticket-type');
+     const carriageNumber = document.querySelector('.carriage');
+     const ticketCode = document.querySelector('.ticket-code');
+     const fullPrice = document.querySelector('.price');
+     const confirm = document.querySelector('.submit');
+     const showFooter = document.querySelector('.hide');
+     
+     const carriage = Math.floor(Math.random()*10)+1;
+     carriageNumber.innerText = carriage;
+
+     const cP = Math.floor(Math.random()*100000)+1;
+     ticketCode.innerText = cP;
+
+     // Controllo di validazione 
+     if (!fullName || !distance || !age) {
+         alert('Per favore, compila tutti i campi.');
+          return; 
+     }
+     const nameParts = fullName.split(' ');
+     if (nameParts.length < 2) {
+         alert('Per favore, inserisci sia il nome che il cognome.');
+        return;
+     }
+     
 
      const priceCalculator = (km, ageRange) => {
         if (isNaN(km) || km <= 0) {
@@ -21,10 +46,27 @@ form.addEventListener('submit', (event) => { //prevengo l'invio del form prima c
         } 
         return price.toFixed(2); // Formatto a due cifre decimali con toFixed()
         }
-    console.log(priceCalculator(distance, age));
+        fullPrice.innerText =`${priceCalculator(distance, age)} € tasse comprese` ;
     
-    form.reset();
+        ticketName.innerText = fullName.trim();
+        if(age === 'u18'){
+            ticketType.innerText = 'Sconto Under 18';
+        } else if(age === 'senior'){
+            ticketType.innerText = 'Sconto Over 60';
+        } else{
+            ticketType.innerText = 'Biglietto standard';
+        }
+        confirm.value = 'Conferma';
+        showFooter.classList.remove('hide');
+        
+        form.reset();
+        
+
+
 });
+
+
+
 
 
 
